ae9effda4e1aa27725bc3da39800f3344512141b
vuT <xrxratny@gmail.com>
2023-06-02 10:46:20 +0200

    new file created for gui generator

---
+++ GUI_Generator.py
@@ -0,0 +1,9 @@
+import numpy as np
+from PIL import Image, ImageDraw
+#Wat je moet doen: Genereer een foto van 480px by 320px. Waarin alle onderdelen moeten zijn, een voorbeeld staat in de onedrive onder
+#Deelopdrachten\9 - Tekeningen\Dashboard\Dashboard UIs
+#de output van de function moet een bitmap zijn (een 3D numpy array.) (X image, Y image, (R,G,B))
+#De inputs zijn: 
+
+def GenerateUI(speed:float,measurement:int,measuretime:str,satteliteinfo:str,CCstate:bool,CCspeed:float): 
+    print('hier code')
